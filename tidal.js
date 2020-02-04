2565fbd413aa6c31740c64f7d1c083cb761754b5
rustysec <russ@infocyte.com>
2020-02-03 21:07:04 -0800

    real fix for the highlight bug

--- tidal.js
+++ tidal.js
@@ -96,7 +96,7 @@ var Tidal = class TidalClass {
                                 .get_monitor_scale(meta.get_monitor());
 
                         if (highlight && (!this.activeHighlight || !this.activeHighlight.show)) {
-                            this.activeHighlight = new ActiveHighlight(this._settings);
+                            this.activeHighlight = new ActiveHighlight(this.settings);
                         }
                         this.activeHighlight.window = meta;
                         this.activeHighlight.show();
